e07e25b068aa3872b7661177b0a577ce404a67ea
alexcirilo <c.alex.cirilo@gmail.com>
2022-05-31 16:13:18 -0300

    ajuste conexão

--- connection/conexao.php
+++ connection/conexao.php
@@ -1,16 +1,16 @@
 <?php
-/*
+
 $host= "us-cdbr-east-05.cleardb.net";
 $user = "b05a4271466144";
 $pwd = "76bbb149";
 $database = "heroku_41029b0e9a222e3";
-*/
 
+/*
 $host= "localhost";
 $user = "root";
 $pwd = "";
 $database = "connect_pet";
-
+*/
 /**
  * heroku access:
  * mysql://b05a4271466144:76bbb149@us-cdbr-east-05.cleardb.net/heroku_41029b0e9a222e3?reconnect=true
